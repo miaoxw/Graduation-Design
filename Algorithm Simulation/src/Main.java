5ec21be3c98d3f557c2df2772c50071f8841f66b
Miau Shiauwe <mxw_19940830@sina.com>
2016-03-11 15:21:17 +0800

    Algorithm Exploration

--- Algorithm Simulation/src/Main.java
+++ Algorithm Simulation/src/Main.java
@@ -4,7 +4,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.security.acl.LastOwnerException;
 import java.text.DecimalFormat;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main
@@ -16,19 +18,14 @@ public class Main
 	
 	private static boolean rising=false;
 	
-	//Parameters to adjust
-	public static final double PEDOMETER_THRESHOLD_LOWER_BOUND=1.08;
+	// Parameters to adjust
+	public static final double PEDOMETER_THRESHOLD_LOWER_BOUND=1.2;
 	public static final double PEDOMETER_THRESHOLD_UPPER_BOUND=4;
 	public static final double PEDOMETER_GRAVITY_BASIS=1.0;
 	public static final long FOOTSTEP_TIME_LOWER_BOUND=13;
 	public static final long FOOTSTEP_TIME_UPPER_BOUND=100;
 	
-	public static double accelerationRecord[]=new double[150];
-	public static double lastAcceleration;
-	public static double threshold;
-	public static double thresholdCeil;
-	public static double accelerationHistory[]=new double[32];
-	public static int count;
+	public static LinkedList<Double> accelerationRecord;
 	public static long lastPeakTick;
 	public static long tick;
 	
@@ -44,7 +41,7 @@ public class Main
 			writtenFile.createNewFile();
 			outputStream=new FileOutputStream(writtenFile);
 			fileWriter=new OutputStreamWriter(outputStream,"utf-8");
-			fileWriter.write("原始加速度,平滑加速度,threshold,计步,threshold上阈值\r\n");
+			fileWriter.write("原始加速度,计步\r\n");
 		}
 		catch(FileNotFoundException e)
 		{
@@ -84,14 +81,8 @@ public class Main
 	
 	public static void init()
 	{
-		threshold=PEDOMETER_THRESHOLD_LOWER_BOUND;
-		count=0;
+		accelerationRecord=new LinkedList<>();
 		tick=0;
-		lastAcceleration=PEDOMETER_GRAVITY_BASIS;
-		lastPeakTick=0;
-		
-		for(int i=0;i<32;i++)
-			accelerationHistory[i]=PEDOMETER_GRAVITY_BASIS;
 	}
 	
 	public static boolean judgeFootStep(double acceleration)
@@ -101,76 +92,50 @@ public class Main
 		if(Double.isNaN(acceleration))
 			return false;
 		
-		boolean ret=false;
+		boolean ret=true;
 		
-		for(int i=0;i<31;i++)
-			accelerationHistory[i+1]=accelerationHistory[i];
-		accelerationHistory[0]=acceleration;
-		
-		double filteredAcceleration=0.0;
-		for(int i=0;i<32;i++)
-			filteredAcceleration+=accelerationHistory[i];
-		filteredAcceleration/=32;
-		
-		accelerationRecord[count++]=filteredAcceleration;
-		if(count==150)
-		{
-			count=0;
-			
-			double accTotal=0.0;
-			int peakCount=0;
-			double maxAcceleration=-1e4;
-			for(int i=1;i<149;i++)
-			{
-//				if(accelerationRecord[i]>maxAcceleration)
-//					maxAcceleration=accelerationRecord[i];
-				if(accelerationRecord[i]>accelerationRecord[i-1]&&accelerationRecord[i]<accelerationRecord[i+1]&&accelerationRecord[i]>PEDOMETER_GRAVITY_BASIS)
-				{
-					accTotal+=accelerationRecord[i];
-					peakCount++;
-				}
-			}
-			
-//			threshold=maxAcceleration*0.25+PEDOMETER_GRAVITY_BASIS*0.75;
-//			if(threshold<PEDOMETER_THRESHOLD_LOWER_BOUND)
-//				threshold=PEDOMETER_THRESHOLD_LOWER_BOUND;
-//			updateThresholdCeil();
-			threshold=accTotal/peakCount;
-			if(threshold<PEDOMETER_THRESHOLD_LOWER_BOUND)
-				threshold=PEDOMETER_THRESHOLD_LOWER_BOUND;
-		}
-		
-		if(filteredAcceleration>=lastAcceleration)
-			rising=true;
+		accelerationRecord.add(acceleration);
+		if(accelerationRecord.size()<41)
+			ret=false;
 		else
 		{
-			if(lastAcceleration>=threshold&&rising)
+			if(accelerationRecord.size()>41)
+				accelerationRecord.removeFirst();
+			
+			// 寻潜在波峰，剔除休息状态
+			ret=accelerationRecord.get(20)>accelerationRecord.get(19)&&accelerationRecord.get(20)>accelerationRecord.get(21)&&accelerationRecord.get(20)>=PEDOMETER_THRESHOLD_LOWER_BOUND;
+			
+			// 阈值判断
+			if(ret)
 			{
-				long currentTick=tick;
-				long deltaTick=currentTick-lastPeakTick;
-				
-				if(deltaTick>FOOTSTEP_TIME_LOWER_BOUND&&deltaTick<FOOTSTEP_TIME_UPPER_BOUND)
+				boolean isPeak=true;
+				// i==1已经在第一步判定过
+				for(int i=2;i<=12;i++)
+					if(accelerationRecord.get(20)<accelerationRecord.get(20-i)||accelerationRecord.get(20)<accelerationRecord.get(20+i))
+					{
+						isPeak=false;
+						break;
+					}
+				if(tick-20-lastPeakTick>=FOOTSTEP_TIME_LOWER_BOUND&&tick-20-lastPeakTick<=FOOTSTEP_TIME_UPPER_BOUND)
 					ret=true;
-				
-				lastPeakTick=currentTick;
+				else
+					ret=false;
+				ret=ret&&isPeak;
+				if(isPeak)
+					lastPeakTick=tick-20;
 			}
-			rising=false;
-		}
-		
-		lastAcceleration=filteredAcceleration;
-		try
-		{
-			DecimalFormat format=new DecimalFormat("0.######");
-			fileWriter.write(format.format(acceleration)+",");
-			fileWriter.write(format.format(filteredAcceleration)+",");
-			fileWriter.write(format.format(threshold)+",");
-			fileWriter.write((ret?1:0)+",");
-			fileWriter.write(format.format(thresholdCeil)+"\r\n");
-		}
-		catch(IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			
+			try
+			{
+				DecimalFormat format=new DecimalFormat("0.######");
+				fileWriter.write(format.format(accelerationRecord.get(20))+",");
+				fileWriter.write((ret?1:0)+"\r\n");
+			}
+			catch(IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return ret;
 	}
