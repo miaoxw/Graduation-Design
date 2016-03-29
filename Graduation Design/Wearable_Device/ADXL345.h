7fc3194ec127c7ef7b48fccf7a5a044620ce915d
Miau Shiauwe <mxw_19940830@sina.com>
2016-03-29 12:28:11 +0800

    Code style changes

--- Graduation Design/Wearable_Device/ADXL345.h
+++ Graduation Design/Wearable_Device/ADXL345.h
@@ -3,24 +3,24 @@
 
 namespace ADXL345
 {
-	const double SCALE = 64.0;
+	const double SCALE=64.0;
 
-	const uint8_t address = 0x53;
-	const uint8_t OFSX = 0x1E;
-	const uint8_t OFSY = 0x1F;
-	const uint8_t OFSZ = 0x20;
-	const uint8_t BW_RATE = 0x2C;
-	const uint8_t POWER_CTL = 0x2D;
-	const uint8_t INT_ENABLE = 0x2E;
-	const uint8_t INT_MAP = 0x2F;
-	const uint8_t INT_SOURCE = 0x30;
-	const uint8_t DATA_FORMAT = 0x31;
+	const uint8_t address=0x53;
+	const uint8_t OFSX=0x1E;
+	const uint8_t OFSY=0x1F;
+	const uint8_t OFSZ=0x20;
+	const uint8_t BW_RATE=0x2C;
+	const uint8_t POWER_CTL=0x2D;
+	const uint8_t INT_ENABLE=0x2E;
+	const uint8_t INT_MAP=0x2F;
+	const uint8_t INT_SOURCE=0x30;
+	const uint8_t DATA_FORMAT=0x31;
 
-	const uint8_t X_low = 0x32;
-	const uint8_t Y_low = 0x34;
-	const uint8_t Z_low = 0x36;
+	const uint8_t X_low=0x32;
+	const uint8_t Y_low=0x34;
+	const uint8_t Z_low=0x36;
 
-	const uint8_t THRESHOLD_0G = 5u;
+	const uint8_t THRESHOLD_0G=5u;
 };
 
 #endif
--- Graduation Design/Wearable_Device/counter.cpp
+++ Graduation Design/Wearable_Device/counter.cpp
@@ -12,78 +12,78 @@ namespace Pedometer
 	unsigned long lastPeakTick;
 	unsigned long tick;
 
-	const double PEDOMETER_THRESHOLD_LOWER_BOUND = 1.02;
-	const double PEDOMETER_GRAVITY_BASIS = 1.0;
+	const double PEDOMETER_THRESHOLD_LOWER_BOUND=1.02;
+	const double PEDOMETER_GRAVITY_BASIS=1.0;
 }
 
 void Pedometer::init()
 {
-	threshold = PEDOMETER_THRESHOLD_LOWER_BOUND;
-	count = 0;
-	tick = 0;
-	lastAcceleration = 1.0;
-	lastPeakTick = 0;
+	threshold=PEDOMETER_THRESHOLD_LOWER_BOUND;
+	count=0;
+	tick=0;
+	lastAcceleration=1.0;
+	lastPeakTick=0;
 
-	for (int i = 0; i < 16; i++)
-		accelerationHistory[i] = 1.0;
+	for(int i=0; i<16; i++)
+		accelerationHistory[i]=1.0;
 }
 
 bool Pedometer::judgeFootstep(double acceleration)
 {
 	tick++;
 
-	if (isnan(acceleration))
+	if(isnan(acceleration))
 		return false;
 
-	bool ret = false;
+	bool ret=false;
 	static bool rising;
 
 	//均值滤波，窗口大小已经扩充至16
-	for (int i = 0; i < 15; i++)
-		accelerationHistory[i + 1] = accelerationHistory[i];
-	accelerationHistory[0] = acceleration;
+	for(int i=0; i<15; i++)
+		accelerationHistory[i+1]=accelerationHistory[i];
+	accelerationHistory[0]=acceleration;
 
-	double filteredAcceleration = 0.0;
-	for (int i = 0; i < 16; i++)
-		filteredAcceleration += accelerationHistory[i];
-	filteredAcceleration /= 16;
+	double filteredAcceleration=0.0;
+	for(int i=0; i<16; i++)
+		filteredAcceleration+=accelerationHistory[i];
+	filteredAcceleration/=16;
 
 	//此处存入滤波后的数据
-	accelerationRecord[count++] = filteredAcceleration;
+	accelerationRecord[count++]=filteredAcceleration;
 
-	if (count == 150u)
+	if(count==150u)
 	{
-		count = 0;
+		count=0;
 
-		double maxAcceleration = -1e4;
-		for (int i = 0; i < 150; i++)
+		double maxAcceleration=-1e4;
+		for(int i=0; i<150; i++)
 		{
-			if (accelerationRecord[i] > maxAcceleration)
-				maxAcceleration = accelerationRecord[i];
+			if(accelerationRecord[i]>maxAcceleration)
+				maxAcceleration=accelerationRecord[i];
 		}
 
-		threshold = (maxAcceleration + PEDOMETER_GRAVITY_BASIS) / 2;
-		if (threshold < PEDOMETER_THRESHOLD_LOWER_BOUND)
-			threshold = PEDOMETER_THRESHOLD_LOWER_BOUND;
+		threshold=(maxAcceleration+PEDOMETER_GRAVITY_BASIS)/2;
+		if(threshold<PEDOMETER_THRESHOLD_LOWER_BOUND)
+			threshold=PEDOMETER_THRESHOLD_LOWER_BOUND;
 	}
 
-	if (filteredAcceleration >= lastAcceleration)
-		rising = true;
+	if(filteredAcceleration>=lastAcceleration)
+		rising=true;
 	else
 	{
-		if (lastAcceleration >= threshold&&rising)
+		if(lastAcceleration>=threshold&&rising)
 		{
-			unsigned long currentTick = tick;
-			unsigned long deltaTick = currentTick - lastPeakTick;
+			unsigned long currentTick=tick;
+			unsigned long deltaTick=currentTick-lastPeakTick;
 
-			if (deltaTick > FOOTSTEP_TIME_LOWER_BOUND&&deltaTick < FOOTSTEP_TIME_UPPER_BOUND)
-				ret = true;
+			if(deltaTick>FOOTSTEP_TIME_LOWER_BOUND&&deltaTick<FOOTSTEP_TIME_UPPER_BOUND)
+				ret=true;
 
-			lastPeakTick = currentTick;
+			lastPeakTick=currentTick;
 		}
-		rising = false;
+		rising=false;
 	}
 
-	lastAcceleration = filteredAcceleration;
+	lastAcceleration=filteredAcceleration;
 	return ret;
 }
